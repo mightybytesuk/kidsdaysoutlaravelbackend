61bcf4d8fd82e69c9ddb0d374602d63cf2929be8
Aiden <84194083+mightybytesuk@users.noreply.github.com>
2022-08-12 21:48:59 +0100

    Update Add Listing View - Admin Panel

--- resources/views/admin/addListing.blade.php
+++ resources/views/admin/addListing.blade.php
@@ -5,8 +5,12 @@
     <form action="" method="post">
         <input type="text" name="name" placeholder="Name" class="form-control">
         <textarea name="description" class="form-control" cols="30" rows="10" placeholder='Description' required></textarea>
-        <input type="text" name="long" class="form-control" placeholder="Long">
-        <input type="text" name="lat" class="form-control" placeholder="lat">
+        <input type="text" name="line_one" class="form-control" placeholder="Line One">
+        <input type="text" name="line_two" class="form-control" placeholder="Line Two">
+        <input type="text" name="town" class="form-control" placeholder="Town">
+        <input type="text" name="county" class="form-control" placeholder="County">
+        <input type="text" name="country" class="form-control" placeholder="Country">
+        <input type="text" name="postcode" class="form-control" placeholder="Postcode/Zip Code">
         <input type="text" name="price" class="form-control" placeholder="Price">
         <input type="text" name="website" class="form-control" placeholder="Website URL">   
         <input type="file" name="image" class="form-control">
